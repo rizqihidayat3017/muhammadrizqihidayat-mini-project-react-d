4973f77e6916212cbd380f4279d9d923b9e852e8
rizqihidayat3017 <rizqihidayat1234@gmail.com>
2022-05-17 09:21:11 +0700

    add data list project

--- rizqihidayat-mini-project-d/src/components/Pagenotfound.jsx
+++ rizqihidayat-mini-project-d/src/components/Pagenotfound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Pagenotfound() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
+    <div className="flex items-center justify-center h-screen">
       <div className="px-4 lg:py-12">
         <div className="lg:gap-4 lg:flex">
           <div className="flex flex-col items-center justify-center md:py-24 lg:py-32">
