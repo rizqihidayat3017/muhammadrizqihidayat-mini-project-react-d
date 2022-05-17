310008c17ab6c0813084d651d980fe10c4d3cc57
rizqihidayat3017 <rizqihidayat1234@gmail.com>
2022-05-17 16:24:45 +0700

    #

--- rizqihidayat-mini-project-d/src/components/Navbar.jsx
+++ rizqihidayat-mini-project-d/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Outlet, Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -7,7 +8,7 @@ function Navbar() {
         <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
           <div className="relative flex flex-col md:flex-row">
             <a
-              href="/"
+              to="/"
               className="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0"
             >
               <span className="mx-auto text-xl font-black leading-none text-white select-none hover:text-indigo-700">
@@ -16,25 +17,25 @@ function Navbar() {
             </a>
             <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
               <a
-                href="/beranda"
+                to="/beranda"
                 className="mr-5 font-medium leading-6 text-white hover:text-indigo-700"
               >
                 Beranda
               </a>
               <a
-                href="/organisasi"
+                to="/organisasi"
                 className="mr-5 font-medium leading-6 text-white hover:text-indigo-700"
               >
                 Organisasi
               </a>
               <a
-                href="/project"
+                to="/project"
                 className="mr-5 font-medium leading-6 text-white hover:text-indigo-700"
               >
                 Portofolio
               </a>
               <a
-                href="/kepanitiaan"
+                to="/kepanitiaan"
                 className="mr-5 font-medium leading-6 text-white hover:text-indigo-700"
               >
                 Kepanitiaan
@@ -43,13 +44,14 @@ function Navbar() {
           </div>
           <div className="inline-flex items-center ml-5 space-x-6 lg:justify-end">
             <a
-              href="#_"
+              to="#_"
               className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-primary border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
             >
               Masuk
             </a>
           </div>
         </div>
+        {/* <Outlet /> */}
       </section>
     </div>
   );
